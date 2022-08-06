ac642754fb183e98d0b47e0e9fc605c42fbeea8a
Михаил Ячменьков <mik1998@MacBook-Pro-Mihail.local>
2022-08-06 22:57:41 +0300

    Fix func greeting in main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,26 @@
 """Генератор приветствий."""
 import pprint
 
+
 def greeting(name: str) -> str:
-      """Возвращает текст приветствия.
+    """Возвращает текст приветствия.
 
-      Args:
-          name: Имя пользователя
+    Args:
+        name: Имя пользователя
 
-      Returns:
-          int: Текст приветствия
-      """
-      # pprint.pprint(name.lower())
-      return 'Привет, name'
+    Returns:
+        str: Текст приветствия
+    """
+    if not isinstance(name, str):
+        name = str(name)
+    single_word_list = name.split()
+    if len(single_word_list) > 1:
+        temp_valid_name_list = []
+        for word in single_word_list:
+            temp_valid_name_list.append(word.capitalize())
+        name = ' '.join(temp_valid_name_list)
+    else:
+        name = name.capitalize()
+    pp = pprint.PrettyPrinter()
+    pp.pprint(name.lower())
+    return 'Привет, {name}'.format(name=name)
